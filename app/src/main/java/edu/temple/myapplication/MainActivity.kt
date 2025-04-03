68218025d23c773e09a8ed819c58dcc4ae3f74e8
Kostandin Jorgji <tug74295@temple.edu>
2025-04-03 12:05:03 -0400

    Add handler and textview to update

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,17 +5,25 @@ import android.content.Intent
 import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var timerTextView: TextView
     lateinit var timerBinder: TimerService.TimerBinder
     var isConnected = false
 
+    val handler = Handler(Looper.getMainLooper()) {
+        timerTextView.text = it.what.toString()
+        true
+    }
     val serviceConnection = object : ServiceConnection {
         override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
             timerBinder = service as TimerService.TimerBinder
+            timerBinder.setHandler(handler)
             isConnected = true
         }
 
@@ -23,18 +31,15 @@ class MainActivity : AppCompatActivity() {
             isConnected = false
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        timerTextView = findViewById(R.id.textView)
         findViewById<Button>(R.id.startButton).setOnClickListener {
             if (isConnected) timerBinder.start(
                 startValue = 100
             )
-        }
-
-        findViewById<Button>(R.id.pauseButton).setOnClickListener {
-            if (isConnected) timerBinder.pause()
         }
 
         findViewById<Button>(R.id.stopButton).setOnClickListener {
